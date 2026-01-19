4f5d279ab22323ea0f00e4035bff1c4835596b0a
Chimbori <hello@chimbori.com>
2026-01-19 07:59:30 -0800

    Avoid double-nested HTMX-replaced `<div>` in the domains list

--- src/dashboard/domains_templ.go
+++ src/dashboard/domains_templ.go
@@ -32,7 +32,7 @@ func AuthorizedDomainsTempl(domains []db.Domain) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<table class=\"dashboard w-full\" id=\"authorized-domains\" hx-target=\"#authorized-domains\" hx-swap=\"innerHTML transition:true\"><tr><th>Domain</th><th class=\"text-center\">Include Subdomains</th><th>Updated</th><th>Actions</th></tr>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<table class=\"dashboard w-full\" id=\"authorized-domains\" hx-target=\"#authorized-domains\" hx-swap=\"outerHTML transition:true\"><tr><th>Domain</th><th class=\"text-center\">Include Subdomains</th><th>Updated</th><th>Actions</th></tr>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
